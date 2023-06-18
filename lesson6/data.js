b4a32beba413310c71782c970366c5c5f17cc15a
Maks <kpasowsky@gmail.com>
2023-06-18 03:51:54 +0300

    hm

---
+++ lesson6/data.js
@@ -0,0 +1,52 @@
+const dataProducts = `
+[
+    {
+        "image": "img/foto1.jpg",
+        "title": "MANGO  PEOPLE  T-SHIRT",
+        "price": "300",
+        "color": "Red",
+        "size": "Xl",
+        "Quantity": "2"
+    },
+    {
+        "image": "img/foto2.jpg",
+        "title": "ELLERY X M'O CAPSULE",
+        "price": "52",
+        "color": "Black",
+        "size": "Xl",
+        "Quantity": "1"
+    },
+    {
+        "image": "img/foto1.jpg",
+        "title": "MANGO  PEOPLE  T-SHIRT",
+        "price": "300",
+        "color": "Red",
+        "size": "Xl",
+        "Quantity": "2"
+    },
+    {
+        "image": "img/foto2.jpg",
+        "title": "ELLERY X M'O CAPSULE",
+        "price": "52",
+        "color": "Black",
+        "size": "Xl",
+        "Quantity": "1"
+    },
+    {
+        "image": "img/foto1.jpg",
+        "title": "MANGO  PEOPLE  T-SHIRT",
+        "price": "300",
+        "color": "Red",
+        "size": "Xl",
+        "Quantity": "2"
+    },
+    {
+        "image": "img/foto2.jpg",
+        "title": "ELLERY X M'O CAPSULE",
+        "price": "52",
+        "color": "Black",
+        "size": "Xl",
+        "Quantity": "1"
+    }
+]
+`
