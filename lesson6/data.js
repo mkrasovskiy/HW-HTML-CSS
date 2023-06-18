71ea82c5564ccd67a42a948998880d27096840ad
Maks <kpasowsky@gmail.com>
2023-06-18 14:43:26 +0300

    Пробный вариант

--- lesson6/data.js
+++ lesson6/data.js
@@ -1,5 +1,4 @@
 const dataProducts = `
-[
     {
         "image": "img/foto1.jpg",
         "title": "MANGO  PEOPLE  T-SHIRT",
@@ -7,46 +6,5 @@ const dataProducts = `
         "color": "Red",
         "size": "Xl",
         "Quantity": "2"
-    },
-    {
-        "image": "img/foto2.jpg",
-        "title": "ELLERY X M'O CAPSULE",
-        "price": "52",
-        "color": "Black",
-        "size": "Xl",
-        "Quantity": "1"
-    },
-    {
-        "image": "img/foto1.jpg",
-        "title": "MANGO  PEOPLE  T-SHIRT",
-        "price": "300",
-        "color": "Red",
-        "size": "Xl",
-        "Quantity": "2"
-    },
-    {
-        "image": "img/foto2.jpg",
-        "title": "ELLERY X M'O CAPSULE",
-        "price": "52",
-        "color": "Black",
-        "size": "Xl",
-        "Quantity": "1"
-    },
-    {
-        "image": "img/foto1.jpg",
-        "title": "MANGO  PEOPLE  T-SHIRT",
-        "price": "300",
-        "color": "Red",
-        "size": "Xl",
-        "Quantity": "2"
-    },
-    {
-        "image": "img/foto2.jpg",
-        "title": "ELLERY X M'O CAPSULE",
-        "price": "52",
-        "color": "Black",
-        "size": "Xl",
-        "Quantity": "1"
     }
-]
 `
